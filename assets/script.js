84f6cb205ed3c7ed25e74220fb0277f4a983e7e8
Evan Keller <evankeller@pop-os.localdomain>
2023-11-27 08:42:37 -0700

    weather cards fully functional

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 var latLon = []
 
+var currentWeatherData = []
+
 var weatherData = []
+
+$(document).ready(function () {
+    $('#currentDate').text(`${dayjs().format('MMM D, YYYY')}`)
+})
+
 
 $('#primarySubmit').on('click', function () {
     var cityName = $('#cityInput').val().trim()
@@ -21,11 +28,21 @@ $('#primarySubmit').on('click', function () {
     });
     console.log(cityName);
 
-    $('#searchedCities').append(`<button class="previousSearch" type="button">${cityName}</button>`)
+    $('#searchedCities').append(`<button id='${cityName}' class="previousSearch" type="button">${cityName}</button>`)
 
 })
 
 function fetchWeather() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latLon[0]}&lon=${latLon[1]}&units=imperial&appid=9eb8c6aa4ea4ae6290f1970b6f5629eb`)
+        .then(function (locationCurrentWeather) {
+            return locationCurrentWeather.json()
+                .then(function (currentWeather) {
+                    currentWeatherData.push(currentWeather)
+                });
+        });
+
+
+
     fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${latLon[0]}&lon=${latLon[1]}&limit=5&units=imperial&appid=9eb8c6aa4ea4ae6290f1970b6f5629eb`)
         .then(function (locationWeather) {
             return locationWeather.json()
@@ -34,20 +51,47 @@ function fetchWeather() {
                     //console.log(weather.list);
                     weatherData.push(weather)
                     weatherToday()
+                    forecast()
                     latLon = []
-
                 });
         });
 }
 
 function weatherToday() {
-    console.log(weatherData[0])
-    var icon = weatherData[0].list[0].weather[0].icon
-    $('#icon').attr('src', `https://openweathermap.org/img/w/${icon}.png`)
-    $('#temperature').text(`Temprature: ${weatherData[0].list[0].main.temp} °F`)
-    $('#wind').text(`Wind Speed: ${weatherData[0].list[0].wind.speed} MPH`)
-    $('#humidity').text(`Humidity: ${weatherData[0].list[0].main.humidity} %`)
+    console.log(currentWeatherData[0])
+    var currentIcon = currentWeatherData[0].weather[0].icon
+    $('#currentIcon').attr('src', `https://openweathermap.org/img/w/${currentIcon}.png`)
+    $('#temperature').text(`Temprature: ${currentWeatherData[0].main.temp} °F`)
+    $('#wind').text(`Wind Speed: ${currentWeatherData[0].wind.speed} MPH`)
+    $('#humidity').text(`Humidity: ${currentWeatherData[0].main.humidity} %`)
 
     console.log(latLon)
+    currentWeatherData = []
+}
+
+function forecast() {
+    weatherDataIndex = [0, 7, 15, 31, 39]
+    console.log(weatherData[0].list)
+
+    $('.forecast-card').remove()
+
+    for (i = 0; i < weatherDataIndex.length; i++) {
+        $('#forecast').append(`<div class='forecast-card bg-primary text-light'>
+    <h5 id='date${i}'></h5>
+    <img id='icon${i}' src="" alt="">
+    <h6 id='temperature${i}'></h6>
+    <h6 id='wind${i}'></h6>
+    <h6 id='humidity${i}'></h6>
+</div>`)
+
+        var icon = weatherData[0].list[weatherDataIndex[i]].weather[0].icon
+        $(`#date${i}`).text(`${dayjs().add(i + 1, 'day').format('MMM D, YYYY')}`)
+        $(`#icon${i}`).attr('src', `https://openweathermap.org/img/w/${icon}.png`)
+        $(`#temperature${i}`).text(`Temp: ${weatherData[0].list[weatherDataIndex[i]].main.temp} °F`)
+        $(`#wind${i}`).text(`Wind: ${weatherData[0].list[weatherDataIndex[i]].wind.speed} MPH`)
+        $(`#humidity${i}`).text(`Humidity: ${weatherData[0].list[weatherDataIndex[i]].main.humidity} %`)
+
+
+    }
     weatherData = []
 }
